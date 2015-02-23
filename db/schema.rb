eababaedf6cab83d38c35fa9d21089cbba399978
Daniel Harris <bobth3bum@gmail.com>
2015-02-23 09:59:19 -0700

    add courses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223165617) do
+ActiveRecord::Schema.define(version: 20150223165812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string  "title"
+    t.boolean "day",   default: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
