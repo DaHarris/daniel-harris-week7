5d2445354f2d876d9dd59475b301f17ed2e14799
Daniel Harris <bobth3bum@gmail.com>
2015-02-23 10:05:48 -0700

    add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, :notice => "User was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path, :notice => "User was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, :notice => "User was successfully deleted."
+  end
+
+end
