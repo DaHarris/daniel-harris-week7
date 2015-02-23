b819a57eea7819763f7e2f670d48ac279d66ebd8
Daniel Harris <bobth3bum@gmail.com>
2015-02-23 10:18:12 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :users
+  resources :courses
+
+  root 'courses#index'
+
+  get 'signin' => 'sessions#new'
+  post 'signin' => 'sessions#create'
+  get 'signout' => 'sessions#destroy'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
