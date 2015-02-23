c48b66ed7152e376f8d050a579db0e89a0be4ce8
Daniel Harris <bobth3bum@gmail.com>
2015-02-23 10:11:43 -0700

    add users_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,4 +36,10 @@ class UsersController < ApplicationController
     redirect_to root_path, :notice => "User was successfully deleted."
   end
 
+  private
+
+  def users_controller
+    params.require(:user).permit(:user_name, :password, :about)
+  end
+
 end
